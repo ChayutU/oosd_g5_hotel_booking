1863423d11cda4c6ea17e98f3a455dfdd683f32b
Chayut <chayut.cs@mail.kmutt.ac.th>
2016-05-23 18:14:46 +0700

    btn Action Temp

---
+++ src/hotel_project/buttonAction/btnActionTemp.java
@@ -0,0 +1,17 @@
+package hotel_project.buttonAction;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by Chayut on 23-May-16.
+ */
+public class btnActionTemp extends AbstractAction {
+
+    //Don't forget to create Constructor
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Do some thing.
+    }
+}
